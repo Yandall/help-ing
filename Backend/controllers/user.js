1ce2a3dca66b4ecfe4ed45fce730a894f1681bd2
Yandall <49245907+Yandall@users.noreply.github.com>
2021-03-08 15:03:58 -0500

    Update user.js

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,25 +1,4 @@
 const db = require('../services/mongoDB')
-const md5 = require('md5')
-
-async function getUser (req,res)  {
-    const connection = await db.getConnection()
-    try{
-        var filter = {"email" : req.params.email ,"password" : md5(req.params.password)}
-        console.log(filter)
-        let dbo = connection.db('helping')
-        let cursor =  dbo.collection('users').find(filter)
-        let values = await cursor.toArray()
-        console.log(values)
-        res.status(200).send(values)
-    }catch(e) {
-        res.status(500).send('Hubo un error')
-        console.log(e)
-    }finally{
-        if(connection.isConnected()){
-            await connection.close()
-        }
-    }
-}
 
 async function createUser(data) {
     const connection = await db.getConnection()
@@ -39,7 +18,7 @@ async function saveUser(req, res) {
     try {
         let user = {
             nickname: req.body.nickname,
-            password: md5(req.body.password),
+            password: req.body.password,
             email: req.body.email,
             range: parseInt(req.body.range),
             image: req.file.originalname
