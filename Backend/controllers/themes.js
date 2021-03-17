4a80f9519cc54261d44f679cc4aba0346e2b455d
Carlangasv <50808838+Carlangasv@users.noreply.github.com>
2021-03-17 14:41:24 -0500

    Creación de temas

---
+++ Backend/controllers/themes.js
@@ -0,0 +1,50 @@
+const db = require('../services/mongoDB')
+
+async function getTheme(req, res) {
+    const connection = await db.getConnection()
+    try {
+        let dbo = connection.db('helping')
+        let cursor = dbo.collection('themes')
+        let values = await cursor.toArray()
+    } catch (e) {
+        res.status(500).send("Hubo un error")
+        console.error(e)
+    } finally {
+        if(connection.isConnected()){
+            await connection.close()
+        }
+    }
+
+}
+
+
+async function createTheme(data){
+    const connection = await db.getConnection()
+    try{
+        let dbo = connection.db('helping')
+        await dbo.collection("types").insertOne(data)
+    } catch(e){
+        console.error(e)
+    }finally{
+        if(connection.isConnected()) await connection.close()
+    }
+}
+
+async function saveTheme(req, res){
+    try{
+        let theme = {
+            name: req.body.name
+        }
+
+        await createTheme(theme)
+        res.status(200).send('Tema agregado')
+    }catch(e){
+        res.status(500).send("Error al crear")
+    }
+}
+
+module.exports ={
+    createTheme,
+    saveTheme,
+    getTheme
+}
